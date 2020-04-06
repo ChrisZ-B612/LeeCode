0b25dfcf92735ac713b140f4be6596326e9c6051
Chris, Z <hzau.mossad@gmail.com>
2020-04-06 18:07:03 +0800

    [Weekly Contest 178]第1、2题

---
+++ src/weekly-contest/weekly-contest-178/how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/contest/weekly-contest-178/problems/how-many-numbers-are-smaller-than-the-current-number/
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent1 = function (nums) {
+    let arr = nums.slice().sort();
+    let obj = {};
+
+    arr.forEach((num, index) => {
+        obj[num] = (obj[num] === undefined) ? index : obj[num];
+    });
+
+    return nums.map((num) => obj[num]);
+};
+
+var smallerNumbersThanCurrent2 = function (nums) {
+    let arr = [];
+    let obj = {};
+
+    nums.forEach((num) => {
+        arr[num] = (arr[num] || 0) + 1
+    });
+
+    let count = 0;
+    arr.forEach((item, index) => {
+        if (item != undefined) {
+            obj[index] = count;
+            count += item;
+        }
+    });
+
+    return nums.map((num) => obj[num]);
+};
+
+let input = [8, 1, 2, 2, 3];
+
+console.log('NASA: input', input);
+console.log('NASA: output1', smallerNumbersThanCurrent1(input));
+console.log('NASA: output2', smallerNumbersThanCurrent2(input));
---
+++ src/weekly-contest/weekly-contest-178/rank-teams-by-votes.js
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/contest/weekly-contest-178/proDlems/rank-teams-by-votes/
+ * @param {string[]} votes
+ * @return {string}
+ */
+var rankTeams = function(votes) {
+    const getCharCode = (input) => input.charCodeAt(0);
+    const fill0 = (input = 0) => ('0'.repeat(4) + input).slice(-4);
+
+    let obj = {};
+    let dualArr = [];
+    let codeZ = getCharCode('Z');
+    let len = votes[0].length;
+
+
+    for (const vote of votes) {
+        for (let index = 0; index < vote.length; index++) {
+            let letter = vote[index];
+            let arr = (obj[letter] = obj[letter] || []);
+            arr[index] = (arr[index] || 0) + 1;
+        }
+    }
+
+    for (const [letter, arr] of Object.entries(obj)) {
+        for (let index = 0; index < len; index++) {
+            arr[index] = fill0(arr[index]);
+        }
+        arr.push(fill0(codeZ - getCharCode(letter)));
+        dualArr.push(arr);
+    }
+
+    dualArr.sort((a, b) => b.join('') - a.join(''));
+
+    return dualArr.map((arr) => String.fromCharCode(codeZ - arr.pop())).join('');
+};
+
+var votes = ["ABC", "ACB", "ABC", "ACB", "ACB"];
+console.log(`NASA: (${rankTeams(votes)})`);
